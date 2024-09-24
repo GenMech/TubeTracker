20bcc52fa64e1272123b930eea98bede00113485
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-09-25 00:00:37 +0530

    feat: route for data scrape

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        "crawlee",
+        "puppeteer",
+        "playwright",
+        "puppeteer-core",
+        "playwright-core"
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
